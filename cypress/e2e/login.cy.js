f44a99922b498aa613e8dfc251f27d7d197eb084
Nicolas Duarte <nicolascoutoduarte@gmail.com>
2025-10-10 23:58:07 -0300

    Adicionei um teste para validar o login com credenciais inválidas

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,7 +4,21 @@ describe('login', () => {
 
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
-    cy.get('#login-section > .btn').click()
+    cy.contains('button','Entrar').click()
+     
+  
+
     cy.contains('h4','Realizar Transferência').should('be.visible')
+  })
+
+  it('login com dados inválidos deve apresentar mensagem de erro', () => {
+    cy.visit('http://localhost:4000/')
+
+    cy.get('#username').click().type('julio.lima')
+    cy.get('#senha').click().type('1234765')
+    cy.get('#login-section > .btn').click()
+    
+    cy.get('.toast').should('be.visible')
+    cy.get('.toast').should('have.text','Erro no login. Tente novamente.')
   })
 })
