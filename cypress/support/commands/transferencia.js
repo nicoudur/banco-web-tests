5ef44bc821ef5d890a16458820e4f690adbcae81
Nicolas Duarte <nicolascoutoduarte@gmail.com>
2025-10-18 21:33:46 -0300

    Arrumando nomenclatura

--- cypress/support/commands/transferencia.js
+++ cypress/support/commands/transferencia.js
@@ -1,5 +1,5 @@
-Cypress.Commands.add('realizarTransferencia', (contaOrigin, contaDestino, valor) => {
-    cy.selecionarOpcaoNaComboBox('conta-origem', contaOrigin)
+Cypress.Commands.add('realizarTransferencia', (contaOrigem, contaDestino, valor) => {
+    cy.selecionarOpcaoNaComboBox('conta-origem', contaOrigem)
     cy.selecionarOpcaoNaComboBox('conta-destino', contaDestino)
     cy.get('#valor').click().type(valor)
     cy.contains('button', 'Transferir').click()
